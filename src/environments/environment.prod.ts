b901090211b465ba0df3c8fa9cb7abbf6ac4c33d
Giuseppe <giuseacqua6@gmail.com>
2025-06-19 17:00:46 +0200

    feat(Municipal & User): implement CRUD for Municipals and Users [4]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
-  BASE_URL: "https://autismfriendly.overzoom/api"
+  BASE_URL: "https://autismfriendly.overzoom.it/api"
 };
